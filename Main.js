61104808226063830ee0aa36a10de3e84cd181cd
SAURAV CHATTERJEE <saurav.chatterjee@emids.com>
2016-02-03 12:37:09 +0530

    title addition

--- Main.js
+++ Main.js
@@ -6,38 +6,46 @@ scotchApp.config(function ($routeProvider) {
 
       // route for the home page
       .when('/', {
-        templateUrl: 'pages/Home.html',
-        controller: 'mainController'
+        templateUrl: 'Views/Home.html',
+        controller: 'landingPageController'
       })
 
       // route for the about page
       .when('/home', {
-        templateUrl: 'pages/Home.html',
-        controller: 'aboutController'
+        templateUrl: 'Views/Home.html',
+        controller: 'mainController'
       })
 
       // route for the about page
       .when('/about', {
-        templateUrl: 'pages/About.html',
+        templateUrl: 'Views/About.html',
         controller: 'aboutController'
       })
 
       // route for the contact page
       .when('/contact', {
-        templateUrl: 'pages/Contact.html',
+        templateUrl: 'Views/Contact.html',
         controller: 'contactController'
       });
 });
 
 // create the controller and inject Angular's $scope
-scotchApp.controller('mainController', function ($scope) {
+scotchApp.controller('landingPageController', function ($rootScope, $scope) {
+  $scope.message = 'Welcome to the landing Page!';
+  $rootScope.header = "HomePage";
+});
+
+scotchApp.controller('mainController', function ($rootScope,$scope) {
   $scope.message = 'Everyone come and see how good I look!';
+  $rootScope.header = "HomePage";
 });
 
-scotchApp.controller('aboutController', function ($scope) {
+scotchApp.controller('aboutController', function ($rootScope,$scope) {
   $scope.message = 'Look! I am an about page.';
+  $rootScope.header = "About";
 });
 
-scotchApp.controller('contactController', function ($scope) {
+scotchApp.controller('contactController', function ($rootScope,$scope) {
   $scope.message = 'Contact us! JK. This is just a demo.';
+  $rootScope.header = "Contact Us";
 });
